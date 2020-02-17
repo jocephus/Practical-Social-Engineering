09887d46abbe4b0ba45efd53db3317be9d15422f
Joe Gray <jgray@advancedpersistentsecurity.net>
2020-02-16 23:26:04 -0500

    Update data_parser_index.py

--- code/Old_SurveyMonkey/data_parser_index.py
+++ code/Old_SurveyMonkey/data_parser_index.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+
+import re
+
+user_pass = re.compile(r"\S.+username\=(?P<user_name>\S.+)\&(?P<password>\S.+)\sHTTP\S.+")
+log = open("/var/log/apache2/access.log", "r")
+array = []
+
+for l in log:
+    u = user_pass.findall(l)
+    if u:
+        print(u)
+    else:
+        exit
